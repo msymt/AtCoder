dab53ffd120e552e2953e15c4adaa6e9765119fb
msy <nam.nam580@gmail.com>
2021-04-20 22:36:40 +0900

    add abc118 B

---
+++ ABC118/foods.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int> > table(100, vector<int>(100, 0));
+  vector<int> fab(10000, 0);
+  rep(i, n){
+    int k;
+    cin >> k;
+    table[i][0] = k; 
+    for(int j = 1; j < k + 1; j++) {
+      int a;
+      cin >> a;
+      table[i][j] = a;
+    }
+  }
+  rep(i, n) {
+    for(int j = 1; j < table[i][0] + 1; j++) {
+      int a = table[i][j];
+      fab[a]++;
+    }
+  }
+  int cnt = 0;
+  rep(i, 10000) {
+    if(fab[i] == n) cnt++;
+  }
+
+  cout << cnt << endl;
+
+  return 0;
+}
