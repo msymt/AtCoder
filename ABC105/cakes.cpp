af97705f22e925a00ab2740c53168362670ffa16
msy <nam.nam580@gmail.com>
2021-04-25 15:39:31 +0900

    add abc105 b

---
+++ ABC105/cakes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n;
+  cin >> n;
+  int c = 4;
+  int d = 7;
+
+  // cakes: 4
+  rep(i, 26) {
+    // doun : 7
+    rep(j, 16) {
+      int x = c * i + d * j;
+      if(x == n) {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
