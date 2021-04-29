43b7ec4ecdfa97729b55cb3dfbdbb123b1db9b23
msy <nam.nam580@gmail.com>
2021-04-29 14:21:01 +0900

    replace header files

--- ABC095/half.cpp
+++ ABC095/half.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <set>
-#include <vector>
-#include <numeric>
-#include <map>
-#include <cmath>
+#include <bits/stdc++.h>
 #define rep(i, n) for(int i = 0; i < (int)(n); i++)
 #define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
 using namespace std;
