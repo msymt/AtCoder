251c275b65745f1f27a8dab71b1bb23ce2634f06
msy <nam.nam580@gmail.com>
2021-04-30 20:39:09 +0900

    add ABC106 B

---
+++ ABC106/105.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int gcd(int n) {
+  int cnt = 0;
+  for(int i = 1; i < n + 1; i++) {
+    if(n % 2 == 1 && n % i == 0) cnt++;
+  }
+  return cnt;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int ans = 0;
+  for(int i = 1; i < n + 1; i++) {
+    int cnt = gcd(i); 
+    if(cnt == 8) ans++;
+  }
+  cout << ans << endl;
+}
