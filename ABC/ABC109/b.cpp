90042c655eb37ec6d65131b03f5582a99e5c7bae
msy <nam.nam580@gmail.com>
2021-05-04 15:08:50 +0900

    add ABC108 B

---
+++ ABC/ABC109/b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    int a = x2 - x1;
+    int b = y2 - y1;
+    cout << x2 - b << " " << y2 + a << " " << x1 - b << " " << y1 + a << endl;
+    return 0;
+}
