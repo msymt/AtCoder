6f83ab8ca20191d1cc58e2bb02e18861d6a602c1
msy <nam.nam580@gmail.com>
2021-05-06 13:58:43 +0900

    add ABC111 B

---
+++ ABC/ABC111/b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    int ans = n / 111;
+    ans = n / 111;
+    if(n % 111 > 0) ans++;
+    cout << ans * 111 << endl;
+    return 0;
+}
