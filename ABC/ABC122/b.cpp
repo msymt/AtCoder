b6757b9131faaf8ed5d3c13b52028e76a77b5a30
msy <nam.nam580@gmail.com>
2021-05-19 00:20:41 +0900

    add ABC122 B

---
+++ ABC/ABC122/b.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int n,m,c; cin >> n >> m >> c;
+    vector<vector<int> > a(1000, vector<int>(m));
+    vector<int> b(201, 0);
+    rep(i, m) cin >> b[i];
+    
+    rep(i, n) {
+        rep(j, m) {
+            cin >> a[i][j];
+        }
+    }
+    int cnt =0;
+    rep(i, n) {
+        ll tmp = 0;
+        rep(j, m) {
+            tmp += (ll)(a[i][j] * b[j]);
+        }
+        if(tmp + c > 0) cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
