1b4759de0fa62b89fa2b2829bca38db0b43a3e3a
msy <nam.nam580@gmail.com>
2021-05-22 07:39:45 +0900

    add ABC125 A

---
+++ ABC/ABC125/a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int a,b,t;cin>>a>>b>>t;
+    cout << t/a *b << endl;
+    return 0;
+}
