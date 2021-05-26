26b04d863e3882723a778fc701bf9b5db4ed251b
msy <nam.nam580@gmail.com>
2021-05-26 14:21:49 +0900

    add ABC057 A

---
+++ ABC/ABC057/a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int a,b;cin>>a>>b;
+    int ans = (a + b) % 24;
+    cout << ans << endl;
+    return 0;
+}
