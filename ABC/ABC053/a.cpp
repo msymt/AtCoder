56fd6bf94fa80174bfa4a1da798d6c198f5d818a
msy <nam.nam580@gmail.com>
2021-05-27 12:47:30 +0900

    add ABC053 A

---
+++ ABC/ABC053/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int x;cin>>x;
+    string ans = "ABC";
+    if(x>=1200) ans="ARC";
+    cout<<ans<<endl;
+    return 0;
+}
