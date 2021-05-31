06ae64e95ea994a09fff25905ec6a331405bb084
msy <nam.nam580@gmail.com>
2021-05-31 13:06:08 +0900

    add ABC050 A

--- ABC/ABC050/a.cpp
+++ ABC/ABC050/a.cpp
@@ -15,11 +15,12 @@ using ll = long long;
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
-    string s;cin>>s;
-    rep(i, s.size()) {
-        if(s[i] == ',') cout << " ";
-        else cout << s[i];
-    }
-    cout << endl;
+    int a,b;
+    char op;
+    cin>>a>>op>>b;
+    int ans;
+    if(op == '+') ans = a + b;
+    else ans = a - b;
+    cout << ans << endl;
     return 0;
 }
