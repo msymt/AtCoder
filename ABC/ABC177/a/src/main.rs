a8721206d0722abaedb1ca9faa8901548ddfec51
msy <nam.nam580@gmail.com>
2021-07-17 09:35:22 +0900

    add ABC177 A

---
+++ ABC/ABC177/a/src/main.rs
@@ -0,0 +1,22 @@
+use std::cmp::*;
+use proconio::input;
+use proconio::marker::Chars;
+
+fn main() {
+    let (d,t,s):(i32,i32,i32) = read_tuple();
+    if d - t*s <= 0 {
+      println!("Yes");
+    } else {
+      println!("No");
+    }
+}
+
+fn read_tuple<T1: std::str::FromStr, T2: std::str::FromStr, T3: std::str::FromStr>() -> (T1, T2, T3) {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).ok();
+    let mut it = buf.trim().split_whitespace();
+    let x = it.next().unwrap().parse::<T1>().ok().unwrap();
+    let y = it.next().unwrap().parse::<T2>().ok().unwrap();
+    let z = it.next().unwrap().parse::<T3>().ok().unwrap();
+    (x, y, z)
+}
