0a3e3b4a014365cbffcaad7778369ef188a4228f
msy <nam.nam580@gmail.com>
2021-08-08 01:30:46 +0900

    rust config

--- ABC/rust/b/src/main.rs
+++ ABC/rust/b/src/main.rs
@@ -5,25 +5,34 @@ use proconio::marker::Chars;
 fn main() {}
 
 fn read<T: std::str::FromStr>() -> T {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).ok();
-    buf.trim().parse::<T>().ok().unwrap()
-  }
+  let mut buf = String::new();
+  std::io::stdin().read_line(&mut buf).ok();
+  buf.trim().parse::<T>().ok().unwrap()
+}
 
-  fn reads<T: std::str::FromStr>(n: usize) -> Vec<T> {
-    let mut vec: Vec<T> = vec![];
-    for _ in 0 .. n {
-      vec.push(read());
-    }
-    vec
+fn reads<T: std::str::FromStr>(n: usize) -> Vec<T> {
+  let mut vec: Vec<T> = vec![];
+  for _ in 0 .. n {
+    vec.push(read());
   }
+  vec
+}
 
-  fn read_tuple<T1: std::str::FromStr, T2: std::str::FromStr, T3: std::str::FromStr>() -> (T1, T2, T3) {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).ok();
-    let mut it = buf.trim().split_whitespace();
-    let x = it.next().unwrap().parse::<T1>().ok().unwrap();
-    let y = it.next().unwrap().parse::<T2>().ok().unwrap();
-    let z = it.next().unwrap().parse::<T3>().ok().unwrap();
-    (x, y, z)
-  }
+fn read_tuple<T1: std::str::FromStr, T2: std::str::FromStr>() -> (T1, T2) {
+  let mut buf = String::new();
+  std::io::stdin().read_line(&mut buf).ok();
+  let mut it = buf.trim().split_whitespace();
+  let x = it.next().unwrap().parse::<T1>().ok().unwrap();
+  let y = it.next().unwrap().parse::<T2>().ok().unwrap();
+  (x, y)
+}
+
+fn reads_tuple<T1: std::str::FromStr, T2: std::str::FromStr, T3: std::str::FromStr>() -> (T1, T2, T3) {
+  let mut buf = String::new();
+  std::io::stdin().read_line(&mut buf).ok();
+  let mut it = buf.trim().split_whitespace();
+  let x = it.next().unwrap().parse::<T1>().ok().unwrap();
+  let y = it.next().unwrap().parse::<T2>().ok().unwrap();
+  let z = it.next().unwrap().parse::<T3>().ok().unwrap();
+  (x, y, z)
+}
