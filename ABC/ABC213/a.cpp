506d45655861e99e3fe44838d7a3bd17d8a2bb08
msy <nam.nam580@gmail.com>
2021-08-13 06:42:38 +0900

    add ABC213 A

---
+++ ABC/ABC213/a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int a, b; cin >> a >> b;
+    //  a ^ (a ^ c) = a ^ b
+    //  cout << a ^ b << endl;
+    for(int i = 0 ; i < 256; i++){
+        if((a ^ i) == b) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
