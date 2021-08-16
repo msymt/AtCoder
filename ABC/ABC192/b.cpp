0c3fd18b39d2a9b6943fbd29201c38127c91c424
msy <nam.nam580@gmail.com>
2021-08-17 01:50:17 +0900

    add ABC192 B

---
+++ ABC/ABC192/b.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    string s; cin >> s;
+    bool ans = true;
+    for(int i = 0; i < s.size(); i++) {
+        if(i % 2 == 0) {
+            if(!islower(s[i])) {
+                ans = false;
+                break;
+            }
+        } else {
+            if(!isupper(s[i])) {
+                ans = false;
+                break;
+            }
+        }
+    }
+    if(ans) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
