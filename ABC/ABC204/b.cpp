e91b62af11ad05c5f4e7b257ec589aaf05afa823
msy <nam.nam580@gmail.com>
2021-08-22 06:43:20 +0900

    add ABC204 B

---
+++ ABC/ABC204/b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+int n, ans = 0;
+int a[1100];
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> n;
+    rep(i, n) cin >> a[i];
+    rep(i, n) {
+        if(a[i] > 10) {
+            ans += a[i] - 10;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
