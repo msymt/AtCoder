cc312bfb1ef5e06d376aae64149ea77e6b3cd56a
msy <nam.nam580@gmail.com>
2021-09-01 00:43:50 +0900

    add ABC173 B

---
+++ ABC/ABC173/b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+// ←↓→↑
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+int n;
+string s[101000];
+vector<string> v = { "AC", "WA", "TLE", "RE" };
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin >> n;
+    rep(i, n) cin >> s[i];
+    map<string, int> C;
+    rep(i, n) C[s[i]]++;
+    for(auto &x:v) {
+        cout << x.c_str() << " x " << C[x] << endl;
+    }
+    return 0;
+}
