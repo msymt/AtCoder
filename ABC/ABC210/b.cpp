8b516eaa9dff6572f528305bc786a8a27b0ab28d
msy <nam.nam580@gmail.com>
2021-09-10 00:11:10 +0900

    add ABC210 B

---
+++ ABC/ABC210/b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+// ←↓→↑
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int n;
+string s;
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    cin >> n >> s;
+    rep(i, n) {
+        if(s[i] == '1') {
+            if(i % 2 == 0) cout << "Takahashi" << endl;
+            else cout << "Aoki" << endl;
+            break;
+        }
+    }
+    return 0;
+}
