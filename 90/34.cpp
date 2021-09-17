524b4d81ba017073bcba1d9ff1892975417ac076
msy <nam.nam580@gmail.com>
2021-09-17 21:46:55 +0900

    add 34/90

---
+++ 90/34.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+const int n_max = 100100;
+int n, k, ans = 0;
+ll a[n_max] = {0};
+map<int, int> m_map;
+
+int main() {
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    int cnt = 0, search_idx = 1;
+    for(int i = 1; i <= n; i++) {
+        // 最後まで
+        while(search_idx <= n) {
+            // 新しい数，kに達した場合
+            if(m_map[a[search_idx]] == 0 && cnt == k) break;
+            // 新しい値
+            if(m_map[a[search_idx]] == 0) cnt++;
+            // 見た値を追加
+            m_map[a[search_idx]]++;
+            // 追加
+            search_idx++;
+        }
+        // 通過区間分を減らす
+        ans = max(ans, search_idx - i);
+        if(m_map[a[i]] == 1) cnt--;
+        m_map[a[i]] -= 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
