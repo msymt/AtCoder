b647c952312641d7c23cffea5d43ceca8af32637
msy <nam.nam580@gmail.com>
2021-09-23 00:35:17 +0900

    add ABC164 B

---
+++ ABC/ABC164/b.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+// ←↓→↑
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    int a,b,c,d; cin >> a >> b >> c >> d;
+    while(1) {
+        c -= b;
+        if(c <= 0) {
+            cout << "Yes" << endl;
+            break;
+        }
+        a -= d;
+        if(a <= 0) {
+            cout << "No" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
