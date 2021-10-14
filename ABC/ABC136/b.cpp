a7db3265e54457d0f01df6ab2a6d9d57f8511e8f
msy <nam.nam580@gmail.com>
2021-10-15 01:20:11 +0900

    add ABC136 B

---
+++ ABC/ABC136/b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+// ←↓→↑
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    int n; cin >> n;
+    int ans = 0;
+    for(int i = 1; i <= n; i++) {
+        if(1 <= i && i <=9) ans++;
+        else if(100 <= i && i <= 999) ans++;
+        else if(10000 <= i && i <= 99999) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
