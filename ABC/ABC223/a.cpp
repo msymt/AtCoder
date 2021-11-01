4651dc879c9280e88faf68dca1223f0a7061ee7d
msy <nam.nam580@gmail.com>
2021-11-02 01:55:12 +0900

    add ABC223 A

---
+++ ABC/ABC223/a.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    int x;cin >> x;
+    if(x % 100 == 0 && x / 100 > 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
