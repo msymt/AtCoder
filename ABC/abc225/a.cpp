8b9a270abbfa7c60c7fe9b410cd2480693be87b9
msy <nam.nam580@gmail.com>
2021-11-07 16:22:31 +0900

    add ABC225 A

---
+++ ABC/abc225/a.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    string s; cin >> s;
+    if(s[0] == s[1] && s[1] == s[2] && s[0] == s[2]) cout << 1 << endl;
+    else if(s[0] != s[1] && s[1] != s[2] && s[0] != s[2]) cout << 6 << endl;
+    else cout << 3 << endl;
+    return 0;
+}
