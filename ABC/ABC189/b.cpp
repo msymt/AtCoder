f94853522220c24e51a6e3057c8b49b8ae04d8a6
msy <nam.nam580@gmail.com>
2021-11-13 12:44:55 +0900

    add ABC189 B

---
+++ ABC/ABC189/b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    int n, x; cin >> n >> x;
+    int v[n], p[n];
+    int res = 0.0;
+    rep(i, n) {
+        cin >> v[i] >> p[i];
+    }
+    rep(i, n) {
+        res += v[i] * p[i];
+        if(res > x * 100) {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
