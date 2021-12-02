f8fa9edcb33a91b10fe9b5858cc59c06b626b131
msy <nam.nam580@gmail.com>
2021-12-03 00:08:53 +0900

    add ABC180 C and ref

---
+++ ABC/ABC180/README.md
@@ -0,0 +1,7 @@
+# ABC180
+
+## C-Cream puff
+
+約数列挙問題
+
+- https://drken1215.hatenablog.com/entry/2020/10/21/194700
---
+++ ABC/ABC180/c.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    ll n; cin >> n;
+    vector<ll> a;
+    for(ll i = 1; i * i <= n; i++) {
+        if(n % i == 0) {
+            a.push_back(i);
+            if(n / i != i) a.push_back(n / i);
+        }
+    }
+    sort(a.begin(), a.end());
+    for(auto v : a) cout << v << endl;
+    return 0;
+}
