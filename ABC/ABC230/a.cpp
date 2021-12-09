eb671ade04855ba26898437f70c5970b7a3485d6
msymt <nam.nam580@gmail.com>
2021-12-09 12:19:22 +0900

    add ARC230 a

---
+++ ABC/ABC230/a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    cin.tie(0);ios_base::sync_with_stdio(0);
+    int n; cin >> n;
+    cout << "AGC0";
+    if(n >= 42) n++;
+    if(n / 10 >= 1) cout << n << endl;
+    else cout << "0" << n << endl;
+    return 0;
+}
