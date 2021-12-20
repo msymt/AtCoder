8744e1203be15e655f1c56421c13d00c5af99666
msymt <nam.nam580@gmail.com>
2021-12-20 01:10:00 +0000

    Sync LeetCode submission - Implement Queue using Stacks (python3)

---
+++ leetcode/problems/implement_queue_using_stacks/solution.py
@@ -0,0 +1,41 @@
+class MyQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        if(self.empty()):
+            return None
+        else:
+            front_element = self.peek()
+            self.queue.pop(0)
+            return front_element
+
+    def peek(self) -> int:
+        if(self.empty()):
+            return None
+        else:
+            return self.top()
+
+    def empty(self) -> bool:
+        if(len(self.queue) == 0):
+            return True
+        else:
+            return False
+    
+    def top(self):
+        if(self.empty()):
+            return None
+        else:
+            return self.queue[0]
+        
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
