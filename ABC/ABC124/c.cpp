1313d89c4c122b7d8a774a5fcc93c48980b7d671
msymt <nam.nam580@gmail.com>
2021-12-31 00:27:36 +0900

    add ARC124 c

---
+++ ABC/ABC124/c.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    string s; cin >> s;
+    string a = "",b = "";
+    rep(i, s.length()) {
+        if(i % 2 == 0){
+            a += "0";
+            b += "1";
+        } else {
+            a += "1";
+            b += "0";
+        }
+    }
+    int a_diff = 0, b_diff = 0;
+    rep(i, s.length()) {
+        if(s[i] != a[i]) a_diff++;
+        if(s[i] != b[i]) b_diff++;
+    }
+    cout << min(a_diff, b_diff) << endl;
+    return 0;
+}
