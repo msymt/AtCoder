3218c888bff0185e79128c261be6f3920f1687c7
msymt <nam.nam580@gmail.com>
2022-01-06 08:42:49 +0900

    add ABC152 c

---
+++ ABC/ABC152/c.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int n; cin >> n;
+    vector<int> p(n); rep(i, n) cin >> p[i];
+    int min_t = 1e6;
+    int ans = 0;
+    rep(i, n) {
+        chmin(min_t, p[i]);
+        if(min_t == p[i]) ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
