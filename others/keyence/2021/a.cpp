49a2993d9c9eb62b67e25e440aeb53439de6a0c4
msymt <nam.nam580@gmail.com>
2022-01-09 00:25:50 +0900

    add keyence2021 a

---
+++ others/keyence/2021/a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+int main() {
+    ll n; cin >> n;
+    ll a[n], b[n], c[n];
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+    ll max_t = a[0];
+    c[0] = a[0] * b[0];
+    for(int i = 1; i < n; i++) {
+        max_t = max(max_t, a[i]); // max ai
+        c[i] = max(c[i - 1], max_t * b[i]);
+    }
+    rep(i, n) cout << c[i] << endl;
+    return 0;
+}
