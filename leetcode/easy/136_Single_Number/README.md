2d45636f22c77975f8671d558235e62da930e223
msymt <nam.nam580@gmail.com>
2022-01-22 18:06:40 +0900

    add leetcode 136

---
+++ leetcode/easy/136_Single_Number/README.md
@@ -0,0 +1,13 @@
+# 136. Single Number
+
+easy
+
+https://leetcode.com/problems/single-number/
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+## 考え方
+
+辞書型を使ってカウントすれば楽．
---
+++ leetcode/easy/136_Single_Number/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n_dict = {}
+        for i in nums:
+            if(i not in n_dict.keys()):
+                n_dict[i] = 1
+            else:
+                n_dict[i] += 1
+        for k,v in n_dict.items():
+            if v == 1:
+                return k
