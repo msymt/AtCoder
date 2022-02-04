f414422cda1f16632d01317c8700ac92598adff5
msymt <nam.nam580@gmail.com>
2022-02-05 01:54:30 +0900

    add ABC236 a

---
+++ ABC/ABC236/a.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    string s; cin >> s;
+    int a, b; cin >> a >> b;
+    char tmp = s[a - 1];
+    s[a - 1] = s[b - 1];
+    s[b - 1] = tmp;
+
+    cout << s << endl;
+    return 0;
+}
