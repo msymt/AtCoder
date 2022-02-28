740fc18bc388c72445a5fbf21c8c0804678a0d3e
msymt <nam.nam580@gmail.com>
2022-03-01 00:15:33 +0900

    add ABC239 a

---
+++ ABC/ABC239/a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    double h; cin >> h;
+    double a = h * (12800000 + h);
+    printf("%.10f\n", sqrt(a));
+    return 0;
+}
