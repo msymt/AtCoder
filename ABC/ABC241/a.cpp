5f948d6dab9f652bb2b64419107cbdf32e25b0b3
msy <nam.nam580@gmail.com>
2022-03-05 01:19:06 +0900

    add ABC241 A

---
+++ ABC/ABC241/a.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    const int n = 9;
+    vector<int> a(n);
+    rep(i, n + 1) {
+        int ai; cin >> ai;
+        a[i] = ai;
+    }
+    int cur = 0;
+    rep(i, 3) {
+        cur = a[cur];
+    }
+    cout << cur << endl;
+    return 0;
+}
