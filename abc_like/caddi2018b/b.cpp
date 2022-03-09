4f5a008f8c220f05cca074cf0c924a7717af96f5
msymt <nam.nam580@gmail.com>
2022-03-10 00:12:45 +0900

    add caddi 2018 b

---
+++ abc_like/caddi2018b/b.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    ll n, h, w; cin >> n >> h >> w;
+    int cnt = 0;
+    rep(i, n) {
+        int ai, bi; cin >> ai >> bi;
+        if(ai >= h && bi >= w) {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
