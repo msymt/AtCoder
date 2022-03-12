b9796482570314b45b885fef2559467a68ae7213
msy <nam.nam580@gmail.com>
2022-03-13 00:52:03 +0900

    [add] SoundHound 2018 a

---
+++ abc_like/SoundHound_2018/a.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int a, b; cin >> a >> b;
+    if(a + b == 15) cout << "+" << endl;
+    else if(a * b == 15) cout << "*" << endl;
+    else cout << "x" << endl;
+    return 0;
+}
