e7f0aebb317933f64917d04dcc9a7a57fc2062ed
msy <nam.nam580@gmail.com>
2022-03-12 13:53:59 +0900

    add Tenka1 Programmer Beginner Contest B

---
+++ abc_like/Tenka1_Programmer_Beginner_Contest/b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <map>
+#include <queue>
+#include <cmath>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for(int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int a, b, k;
+    cin >> a >> b >> k;
+    rep(i, k) {
+        if(i % 2 == 0) {
+            b += a / 2;
+            a /= 2;
+        } else {
+            a += b / 2;
+            b /= 2;
+        }
+    }
+    cout << a << " " << b << endl;
+    return 0;
+}
