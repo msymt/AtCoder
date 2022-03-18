f79d34fe0535468b2b522bf360f8a26a14e6a462
msy <nam.nam580@gmail.com>
2022-03-19 00:07:21 +0900

    [add] atcoder submission: 2022/03/19 00:07:21

---
+++ atcoder/submissions/abc243/b.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+    int a_cnt = 0, b_cnt = 0;
+    rep(i, n) {
+        if (a[i] == b[i]) a_cnt++;
+    }
+    rep(i, n) {
+        rep(j, n) {
+            if (i != j && a[i] == b[j]) b_cnt++;
+        }
+    }
+    cout << a_cnt << endl;
+    cout << b_cnt << endl;
+    return 0;
+}
