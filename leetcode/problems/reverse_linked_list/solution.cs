afb2eacddf621cb5f52d6dfd91008e5fbb81b5fd
msymt <nam.nam580@gmail.com>
2022-03-19 11:05:00 +0000

    Sync LeetCode submission - Reverse Linked List (csharp)

---
+++ leetcode/problems/reverse_linked_list/solution.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode newHead = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
