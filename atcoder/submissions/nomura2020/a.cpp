b9cc665a273897c37e36f32cda477385e10df5b9
msy <nam.nam580@gmail.com>
2022-03-20 00:20:32 +0900

    [add] atcoder submission: 2022/03/20 00:20:32

---
+++ atcoder/submissions/nomura2020/a.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int h1, m1, h2, m2, k;
+    cin >> h1 >> m1 >> h2 >> m2 >> k;
+    cout << 60 * (h2 - h1) + m2 - m1 - k << endl;
+    return 0;
+}
