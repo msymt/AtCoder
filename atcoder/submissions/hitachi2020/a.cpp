a6e318836efd2340ddc3e66e68919005b626138b
msy <nam.nam580@gmail.com>
2022-03-21 00:11:57 +0900

    [add] atcoder submission: 2022/03/21 00:11:57

---
+++ atcoder/submissions/hitachi2020/a.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    string s;
+    cin >> s;
+    string ans = "";
+    for (int i = 0; i < 5; i++) {
+        ans += "hi";
+        if (ans == s) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+    return 0;
+}
