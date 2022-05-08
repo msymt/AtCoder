7cb5eb989459223a22b73be0a5fcd1ef8236c081
msy <nam.nam580@gmail.com>
2022-05-09 00:01:28 +0900

    [add] atcoder submission: 2022/05/09 00:01:28

---
+++ atcoder/submissions/code-formula-2014-qualb/a.cs
@@ -0,0 +1,27 @@
+using System.Security.Cryptography;
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      int a = int.Parse(Console.ReadLine());
+      Console.WriteLine(7 - a);
+      return;
+    }
+  }
+}
