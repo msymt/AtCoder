97b84548b93b11701dd0c05a9d0563ac41bf56d9
msy <nam.nam580@gmail.com>
2022-05-11 23:20:01 +0900

    [add] atcoder submission: 2022/05/11 23:20:01

---
+++ atcoder/submissions/code-festival-2014-qualb/a.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      string[] s = Console.ReadLine().Split(' ');
+      int a = int.Parse(s[0]);
+      int b = int.Parse(s[1]);
+      Console.WriteLine(Math.Max(a, b));
+      return;
+    }
+  }
+}
