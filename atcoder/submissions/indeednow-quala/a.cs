fd9f5e81a5f4d60d7e815ab1125c9b808f701611
msy <nam.nam580@gmail.com>
2022-05-20 00:57:04 +0900

    [add] atcoder submission: 2022/05/20 00:57:04

---
+++ atcoder/submissions/indeednow-quala/a.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      string a = Console.ReadLine();
+      string b = Console.ReadLine();
+      Console.WriteLine(a.Length * b.Length);
+      return;
+    }
+  }
+}
