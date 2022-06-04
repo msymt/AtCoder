3e67c2d474346bc5e8d94f5f1f089fb4227d391d
msy <nam.nam580@gmail.com>
2022-06-04 12:26:40 +0900

    [add] atcoder submission: 2022/06/04 12:26:40

---
+++ atcoder/submissions/abc007/b.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      string ans = Console.ReadLine();
+      if(ans == "a") Console.WriteLine("-1");
+      else Console.WriteLine("a");
+      return;
+    }
+  }
+}
