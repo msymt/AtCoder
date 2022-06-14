94a47ca3d66dd63cb9f9f385a3ac149162aff29b
msy <nam.nam580@gmail.com>
2022-06-15 00:07:36 +0900

    [add] atcoder submission: 2022/06/15 00:07:36

---
+++ atcoder/submissions/arc015/a.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      int n = int.Parse(Console.ReadLine());
+      double ans = (double)(n * 9d / 5d) + 32d;
+      Console.WriteLine(ans);
+      return;
+    }
+  }
+}
