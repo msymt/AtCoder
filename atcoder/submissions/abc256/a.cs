09ab4ab76bed6f700ac770ff0b1e6ab4953bd247
msy <nam.nam580@gmail.com>
2022-06-20 00:47:20 +0900

    [add] atcoder submission: 2022/06/20 00:47:20

---
+++ atcoder/submissions/abc256/a.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Buffers.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AtCoder
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine(1 << int.Parse(Console.ReadLine()));
+      return;
+    }
+  }
+}
