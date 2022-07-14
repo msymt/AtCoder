f3cf6ea3650880c428cc783830a6f89bcf2ab102
msy <nam.nam580@gmail.com>
2022-07-14 12:30:07 +0900

    [add] atcoder submission: 2022/07/14 12:30:06

---
+++ atcoder/submissions/cpsco2019-s1/a.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int n, a;
+    cin >> n >> a;
+    int team_num = n / 3;
+    cout << (a + 1) / 3 << " " << min(a, team_num) << endl;
+    return 0;
+}
