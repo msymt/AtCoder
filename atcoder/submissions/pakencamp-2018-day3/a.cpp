d2aa79835ba54cd53f16db1b915d23acd1156eb8
msy <nam.nam580@gmail.com>
2022-07-16 00:25:27 +0900

    [add] atcoder submission

---
+++ atcoder/submissions/pakencamp-2018-day3/a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int y, m, d;
+    cin >> y >> m >> d;
+    if (m == 12 && d == 25) {
+        cout << y - 2018 << endl;
+    } else {
+        cout << "NOT CHRISTMAS DAY" << endl;
+    }
+    return 0;
+}
