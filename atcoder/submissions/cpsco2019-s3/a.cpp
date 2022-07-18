8a5376d359784f4701b2ff02a04ec5d301964c64
msy <nam.nam580@gmail.com>
2022-07-19 02:52:26 +0900

    [add] atcoder submission: 2022/07/19 02:52:26

---
+++ atcoder/submissions/cpsco2019-s3/a.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'O') {
+            cout << "A";
+        } else if (s[i] == 'A') {
+            cout << "O";
+        } else {
+            cout << s[i];
+        }
+    }
+    cout << endl;
+    return 0;
+}
