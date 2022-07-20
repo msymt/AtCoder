da30648d1c5ff9b2dd73761df230f96900fd3499
msy <nam.nam580@gmail.com>
2022-07-21 00:25:27 +0900

    [add] atcoder submission: 2022/07/23 09:21:06

---
+++ atcoder/submissions/abc261/a.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    cout << max(0, min(r1, r2) - max(l1, l2)) << endl;
+    return 0;
+}
