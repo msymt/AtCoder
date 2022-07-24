3d74eed6370c0890c0fbc8cde87b67376bff0f9f
msy <nam.nam580@gmail.com>
2022-07-24 13:25:22 +0900

    [add] atcoder submission: 2022/07/24 13:25:22

---
+++ atcoder/submissions/dwacon2017-prelims/a.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    int diff = (a + b) - n;
+    if (diff <= 0) {
+        cout << 0 << endl;
+    } else {
+        cout << diff << endl;
+    }
+    return 0;
+}
