52111b219e626d9633256ecdc3749355ff0265ae
msy <nam.nam580@gmail.com>
2022-07-28 01:58:49 +0900

    [add] atcoder submission: 2022/07/28 01:58:49

---
+++ atcoder/submissions/code-thanks-festival-2018/a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, k, n) for (int i = (k); i < (int)(n); i++)
+using namespace std;
+using ll = long long;
+
+template <class T>
+inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const ll INF = 1LL << 60;
+
+int main() {
+    int t, a, b, c, d;
+    cin >> t >> a >> b >> c >> d;
+    int ans = 0;
+    if (a <= t) chmax(ans, b);
+    if (c <= t) chmax(ans, d);
+    if (a + c <= t) chmax(ans, b + d);
+    cout << ans << endl;
+    return 0;
+}
